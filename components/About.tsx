a067369f86b023c36fe9a3cd114fba636c18ff1d
TK <t.koprivnik@gmail.com>
2024-07-29 01:09:20 +0200

    Fix quotes

--- components/About.tsx
+++ components/About.tsx
@@ -18,18 +18,19 @@ const About = () => {
           <br />
           <br />
           Committed to simplicity, efficiency, and continuous learning in all
-          aspects of my life. I don't give a f**k about most things in life, but
-          what I do give a strong f**k about are truth, honesty, respect,
-          fairness, freedom, and people I care the most about. My favourite
-          quote is from Socrates: "The unexamined life is not worth living." My
-          favourite music is "This Bitter Earth" from Dinah Washington. My
-          favourite movie is Interstellar, and second-favourite is Pay it
-          Forward. My favourite book is "The Subtle Art of Not Giving a F*ck" by
-          Mark Manson. I love sports, especially weight-lifting and tennis. I'm
-          a pretty good CS player. I like being quiet. Once in my life, I want
-          to save a dog from a shelter. What else... Oh ... I also strongly
-          believe that we work too much and that, as a species, we need to find
-          some higher purpose than just money.
+          aspects of my life. I don&apos;t give a f**k about most things in
+          life, but what I do give a strong f**k about are truth, honesty,
+          respect, fairness, freedom, and people I care the most about. My
+          favourite quote is from Socrates: &apos;The unexamined life is not
+          worth living.&apos; My favourite music is &apos;This Bitter
+          Earth&apos; from Dinah Washington. My favourite movie is Interstellar,
+          and second-favourite is Pay it Forward. My favourite book is &apos;The
+          Subtle Art of Not Giving a F*ck&apos; by Mark Manson. I love sports,
+          especially weight-lifting and tennis. I&apos;m a pretty good CS
+          player. I like being quiet. Once in my life, I want to save a dog from
+          a shelter. What else... Oh ... I also strongly believe that we work
+          too much and that, as a species, we need to find some higher purpose
+          than just money.
           <br />
         </p>
       </div>

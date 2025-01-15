57e2310f0882e511bffe528d82b24f518fef00f3
TK <t.koprivnik@gmail.com>
2025-01-16 00:02:06 +0100

    Update background

--- components/main/StarsBackground.tsx
+++ components/main/StarsBackground.tsx
@@ -21,7 +21,7 @@ const StarBackground = (props: any) => {
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
-          transparent
+          // transparent
           color="$fff"
           size={0.002}
           sizeAttenuation={true}
